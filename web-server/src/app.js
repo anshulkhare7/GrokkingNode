8cb58a0f3aa37dc91832e0a19e83bde9fb3ad0a8
Anshul <anshulkhare7@gmail.com>
2020-05-18 13:25:17 +0530

    added handlebar template engine

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -2,17 +2,39 @@ const log = console.log
 const express = require('express')
 const path = require('path')
 const publicDir = path.join(__dirname, '../public')
-const app = express()
+const webapp = express()
 
-app.use(express.static(publicDir))
+webapp.set('view engine', 'hbs')
+webapp.use(express.static(publicDir))
 
-app.get('/weather', (req, res) => {
+webapp.get('', (req, res) => {
+    res.render('index', {
+        title: "Home",
+        headerText: "Weather App"
+    })
+})
+
+webapp.get('/about', (req, res) => {
+    res.render('about', {
+        title: "About",
+        headerText: "About Me"
+    })
+})
+
+webapp.get('/help', (req, res) => {
+    res.render('help', {
+        title: "Help",
+        headerText: "FAQ"
+    })
+})
+
+webapp.get('/weather', (req, res) => {
     res.send({
         'temperature': 32,
         'humidity': 100
     })
 });
 
-app.listen(3000, () => {
+webapp.listen(3000, () => {
     log('Server started!');  
 })
