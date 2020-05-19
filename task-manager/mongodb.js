88618b48d471ca59a994cc9287a6ace4f3f7ed1b
Anshul <anshulkhare7@gmail.com>
2020-05-19 11:28:13 +0530

    inserting data

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -8,10 +8,33 @@ MongoClient.connect(connectionURL, {useNewUrlParser: true}, (error, client)=>{
     if(error){
         return console.log('Unable to connect to the database')
     }
+    const db = client.db(databaseName)      
+    
+    insertTask(db)
+    insertUser(db)  
+})
 
-    const db = client.db(databaseName)
+const insertUser = (db) => {
     db.collection('users').insertOne({
         name: 'Anshul', 
         age: 39
+    }, (err, result)=>{
+        if(err){
+            console.log('Unable to insert user')
+        }
+
+        console.log(result.ops)
     })
-})
+}
+
+const insertTask = (db) => {
+    const tasks = [{description: "Fetch the veggies", status: true},{description: "Cook dinner", status: false}]
+
+    db.collection('tasks').insertMany(tasks, (err, result)=> {
+        if(err){
+            console.log('Unable to insert user')
+        }
+
+        console.log(result.ops)
+    })
+}
